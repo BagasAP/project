a630f3015be8f3eb9fd0c9d5f62a7b674cf9fa71
BagasAP <151610185@smkassalaambandung.sch.id>
2017-08-07 21:13:15 +0700

    kinerjaguru

--- resources/views/guru/index.blade.php
+++ resources/views/guru/index.blade.php
@@ -1,28 +1,89 @@
-@extends('layouts.app')
 
-@section('content')
-<div class="container">
-	<div class="row">
-		<div class="col-md-12">
-			<ul class="breadcrumb">
-				<li><a href="{{ url('/home')}}">Dashboard</a></li>
-				<li class="active">Buku</li>
-			</ul>
-			<div class="panel panel-default">
-				<div class="panel-heading">
-					<h2 class="panel-title">Buku</h2>
-				</div>
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Kinerja Guru</title>
+    <link href="../css/bootstrap.min.css" rel="stylesheet" type="text/css" />
+    <link href="../css/font-awesome.min.css" rel="stylesheet" type="text/css" />
+    <link href="../css/ionicons.min.css" rel="stylesheet" type="text/css" />
+    <link href="../css/morris/morris.css" rel="stylesheet" type="text/css" />
+    <link href="../css/jvectormap/jquery-jvectormap-1.2.2.css" rel="stylesheet" type="text/css" />
+    <link href="../css/iCheck/all.css" rel="stylesheet" type="text/css" />
+    <link href="../css/style.css" rel="stylesheet" type="text/css" />
+          <style type="text/css">
+          .footer{
 
-				<div class="panel-body">
-					<p><a class="btn btn-primary" href="{{ url('/admin/books/create')}}"> Tambah</a></p>
-					{!! $html->table(['class'=>'table-striped'])!!}
-				</div>
-			</div>
-		</div>
-	</div>
-</div>
-@endsection
+    text-align:center;
+    padding: 10px;
+    background-color:black;
+    font-weight: bold;
+    bottom: true;
+    color: white;
+}
+          </style>
+      </head>
+      <body class="skin-black">
+        <header class="header">
+            <a class="logo">
+                Kinerja Guru
+            </a>
+            <nav class="navbar navbar-static-top" role="navigation">
 
-@section('scripts')
-{!! $html->scripts()!!}
-@endsection
+                <a href="#" class="navbar-btn sidebar-toggle" data-toggle="offcanvas" role="button">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </a>
+                <div class="navbar-right">
+                    <ul class="nav navbar-nav">
+                        <li class="dropdown">
+                                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
+                                    {{ Auth::user()->name }} <span class="caret"></span>
+                                </a>
+
+                                <ul class="dropdown-menu" role="menu">
+                                    <li>
+                                        <a href="{{ route('logout') }}"
+                                            onclick="event.preventDefault();
+                                                     document.getElementById('logout-form').submit();">
+                                            Logout
+                                        </a>
+
+                                        <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                            {{ csrf_field() }}
+                                        </form>
+                                    </li>
+                                </ul>
+                            </li>
+                            </ul>
+                        </div>
+                    </nav>
+                       </header>
+                    <div class="wrapper row-offcanvas row-offcanvas-left">
+                    <aside class="left-side sidebar-offcanvas">
+                        <section class="sidebar">
+                            <div class="user-panel">
+                                    <div>
+                                    <center><img src="" height="80" width="80" class="img-circle" alt="User Image" style="border: 3px solid white;" /></center>
+                                </div>
+                                @include('layouts.menu')
+                            </div>
+                        </section>
+                    </aside>
+
+             
+
+        <script src="http://ajax.googleapis.com/ajax/libs/jquery/2.0.2/jquery.min.js"></script>
+        <script src="../js/jquery.min.js" type="text/javascript"></script>
+        <script src="../js/jquery-ui-1.10.3.min.js" type="text/javascript"></script>
+        <script src="../js/bootstrap.min.js" type="text/javascript"></script>
+        <script src="../js/plugins/chart.js" type="text/javascript"></script>
+        <script src="../js/plugins/iCheck/icheck.min.js" type="text/javascript"></script>
+        <script src="../js/plugins/fullcalendar/fullcalendar.js" type="text/javascript"></script>
+        <script src="../js/Director/app.js" type="text/javascript"></script>
+        <script src="../js/Director/dashboard.js" type="text/javascript"></script>
+       
+</body>
+</html>
---
+++ resources/views/layouts/menu.blade.php
@@ -0,0 +1,56 @@
+
+                            <ul class="sidebar-menu">
+                                <li class="active">
+                                    <a href="index.php">
+                                        <i class="fa fa-th-large"></i> <span>Menu</span>
+                                    </a>
+                                </li>
+                                
+                                <li class="treeview">
+                                <a href="#">
+                                    <i class="fa fa-user"></i>
+                                    <span>Profile</span>
+                                    <i class="fa fa-angle-left pull-right"></i>
+                                </a>
+                            
+                                </li>
+
+                                <li class="treeview">
+                                <a href="#">
+                                    <i class="fa fa-users"></i>
+                                    <span>Guru</span>
+                                    <i class="fa fa-angle-left pull-right"></i>
+                                </a>
+                                    <ul class="treeview-menu">
+                                        <li><a href="{{route('guru.index')}}"><i class="fa fa-angle-double-right"></i> Data Guru</a></li>
+                                        <li><a href="{{route('guru.create')}}"><i class="fa fa-angle-double-right"></i> Tambah Guru</a></li>
+                                    </ul>
+                                </li>
+
+                                <li class="treeview">
+                                <a href="#">
+                                    <i class="fa fa-star"></i>
+                                    <span>Penilaian</span>
+                                    <i class="fa fa-angle-left pull-right"></i>
+                                </a>
+                                    <ul class="treeview-menu">
+                                        <li><a href="#"><i class="fa fa-angle-double-right"></i> Data Penilaian</a></li>
+                                        <li><a href="#"><i class="fa fa-angle-double-right"></i> Tambah Penilaian</a></li>
+                                    </ul>
+                                </li>
+
+                                <li class="treeview">
+                                <a href="#">
+                                    <i class="fa fa-file-text"></i>
+                                    <span>Laporan</span>
+                                    <i class="fa fa-angle-left pull-right"></i>
+                                </a>
+                                    <ul class="treeview-menu">
+                                        <li><a href="#"><i class="fa fa-angle-double-right"></i> Data Laporan</a></li>
+                                        <li><a href="#"><i class="fa fa-angle-double-right"></i> Tambah Laporan</a></li>
+                                    </ul>
+                                </li>
+
+                               
+
+                            </ul>
